5966f4ea4831b8b82b53b5b17f3e90d6ebd38f9b
Andrés Tobelem <andres.tobelem@gmail.com>
2026-05-01 18:25:41 -0300

    feat(domain): persist human users by handle

--- src/pi_clojure/domain/user.clj
+++ src/pi_clojure/domain/user.clj
@@ -3,3 +3,16 @@
 (defn create-human [handle]
   #:user{:handle handle
          :type :user.type/human})
+
+(defn create-store []
+  (atom {}))
+
+(defn find-by-handle [store handle]
+  (get @store handle))
+
+(defn create-human! [store handle]
+  (let [human-user (create-human handle)]
+    (when (find-by-handle store handle)
+      (throw (ex-info "handle already exists" {:handle handle})))
+    (swap! store assoc handle human-user)
+    human-user))
--- test/pi_clojure/domain/user_test.clj
+++ test/pi_clojure/domain/user_test.clj
@@ -7,3 +7,20 @@
     (is (= #:user{:handle "andres"
                   :type :user.type/human}
            (user/create-human "andres")))))
+
+(deftest create-human-user-in-store
+  (testing "persists a human user with a unique handle"
+    (let [store (user/create-store)
+          created-user (user/create-human! store "andres")]
+      (is (= #:user{:handle "andres"
+                    :type :user.type/human}
+             created-user))
+      (is (= created-user
+             (user/find-by-handle store "andres")))))
+
+  (testing "rejects duplicated handles"
+    (let [store (user/create-store)]
+      (user/create-human! store "andres")
+      (is (thrown-with-msg? clojure.lang.ExceptionInfo
+                            #"handle already exists"
+                            (user/create-human! store "andres"))))))
